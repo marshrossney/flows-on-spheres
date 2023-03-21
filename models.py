mobius mixture; fix HMC normalisation; visualise forces as heatmap

models.py
@@ -10,8 +10,19 @@ import torch
 import torch.linalg as LA
 
 from distributions import SphericalUniformPrior3D
-from transforms import MobiusTransform, RQSplineTransform, RQSplineTransformCircularDomain, C2SplineTransform
-from utils import metropolis_acceptance, effective_sample_size, spherical_mesh, simple_fnn_conditioner
+from transforms import (
+    MobiusMixtureTransform,
+    RQSplineTransform,
+    RQSplineTransformCircularDomain,
+    BSplineTransform,
+)
+from utils import (
+    metropolis_acceptance,
+    effective_sample_size,
+    spherical_mesh,
+    simple_fnn_conditioner,
+    mod_2pi,
+)
 from visualisations import scatter, pairplot
 
 Tensor: TypeAlias = torch.Tensor
@@ -24,17 +35,19 @@ class NormalizingFlowBase(pl.LightningModule):
         self,
         κ: float,
         μ: Tensor,
+        n_layers: int,
         batch_size: int,
         val_batch_size: int,
         init_lr: float = 0.001,
     ):
         super().__init__()
-        
+
         self.κ = κ
         μ = μ if isinstance(μ, torch.Tensor) else torch.tensor(μ, dtype=torch.float32)
         μ.div_(LA.vector_norm(μ))
         self.register_buffer("μ", μ)
-        
+
+        self.n_layers = n_layers
         self.batch_size = batch_size
         self.val_batch_size = val_batch_size
         self.init_lr = init_lr
@@ -42,19 +55,43 @@ class NormalizingFlowBase(pl.LightningModule):
         self.log_norm = math.log(κ / (4 * π * math.sinh(κ)))
 
         self.mesh = spherical_mesh(25)
-        
+
+        # Random global rotations applied after each layer (potentially trainable)
+        self.rotations = torch.nn.Parameter(torch.empty(n_layers).uniform_(0, 2 * π))
+
         self.save_hyperparameters()
         if self.logger is not None and type(self.logger) is TensorBoardLogger:
             self.logger.log_hyperparams(self.hparams)
+
+    def apply_global_rotation(self, xy_or_ϕ: Tensor, layer: int) -> Tensor:
+        *data_shape, coord_dims = xy_or_ϕ.shape
+
+        θ = self.rotations[layer]
+
+        if coord_dims == 1:  # working with angles
+            ϕ = xy_or_ϕ
+            return mod_2pi(ϕ + θ)
+        elif coord_dims == 2:  # working with euclidean coords
+            xy = xy_or_ϕ
+            cosθ, sinθ = θ.cos(), θ.sin()
+            # counter-clockwise rotation
+            R = torch.tensor(
+                [[cosθ, -sinθ], [sinθ, cosθ]],
+                device=xy.device,
+            ).view(*[1 for _ in data_shape], 2, 2)
+            xy.unsqueeze_(dim=-2)
+            return (R * xy).sum(dim=-1)
+        else:
+            raise ValueError("Wrong number of dimensions")
 
     def training_step(self, batch, *_):
         z, logq = batch
         x, ldj = self(z)
         logp = self.log_norm + self.κ * torch.mv(x, self.μ)
-        
+
         kl_div = torch.mean(logq - ldj - logp)
         self.log("loss", kl_div)
-        
+
         return kl_div
 
     def validation_step(self, batch, *_):
@@ -71,7 +108,7 @@ class NormalizingFlowBase(pl.LightningModule):
             return
 
         x = x.to("cpu").detach()
-    
+
         fig = scatter(x)
         self.logger.experiment.add_figure(
             "3d_projection", fig, self.trainer.global_step
@@ -88,15 +125,17 @@ class NormalizingFlowBase(pl.LightningModule):
         logp = self.log_norm + self.κ * torch.mv(x, self.μ)
 
         weights = logp - (logq - ldj)
+
         self.log("hp/acceptance", metropolis_acceptance(weights))
         self.log("hp/ess", effective_sample_size(weights))
-        
+        self.log("hp/kl_div", -torch.mean(weights))
+
     def train_dataloader(self):
         return SphericalUniformPrior3D(self.batch_size)
-    
+
     def val_dataloader(self):
         return SphericalUniformPrior3D(self.val_batch_size)
-        
+
     def test_dataloader(self):
         return self.val_dataloader()
 
@@ -104,7 +143,7 @@ class NormalizingFlowBase(pl.LightningModule):
         return torch.optim.Adam(self.parameters(), lr=self.init_lr)
 
 
-class NormalizingFlowRQS(NormalizingFlowBase):
+class NormalizingFlowRQSRQS(NormalizingFlowBase):
     def __init__(
         self,
         κ: float,
@@ -117,25 +156,22 @@ class NormalizingFlowRQS(NormalizingFlowBase):
         val_batch_size: int,
         init_lr: float,
     ):
-        super().__init__(κ, μ, batch_size, val_batch_size, init_lr)
-        
-        if n_layers % 2 == 1:
-            print("An even number of layers is recommended")
+        super().__init__(κ, μ, n_layers, batch_size, val_batch_size, init_lr)
 
-        self.z_spline = RQSplineTransform([-1, 1], n_spline)
-        self.ϕ_spline = RQSplineTransformCircularDomain(n_spline)
+        self.z_transform = RQSplineTransform([-1, 1], n_spline)
+        self.ϕ_transform = RQSplineTransformCircularDomain(n_spline)
 
-        self.z_spline_params = torch.nn.Parameter(
+        self.z_params = torch.nn.Parameter(
             torch.stack(
-                [self.z_spline.identity_params for _ in range(n_layers)],
+                [self.z_transform.identity_params for _ in range(n_layers)],
                 dim=0,
             )
         )
-        self.ϕ_spline_params = torch.nn.ModuleList(
+        self.ϕ_params = torch.nn.ModuleList(
             [
                 simple_fnn_conditioner(
                     in_features=1,
-                    out_features=self.ϕ_spline.n_params,
+                    out_features=self.ϕ_transform.n_params,
                     hidden_shape=hidden_shape,
                     activation=activation,
                 )
@@ -144,7 +180,6 @@ class NormalizingFlowRQS(NormalizingFlowBase):
         )
 
     def forward(self, inputs: Tensor) -> tuple[Tensor, Tensor]:
-
         x, y, z = inputs.split(1, dim=-1)
         ldj = torch.zeros(inputs.shape[0], device=inputs.device)
 
@@ -154,16 +189,18 @@ class NormalizingFlowRQS(NormalizingFlowBase):
         ϕ = torch.atan2(v, u)  # [-π, π)
         ϕ = torch.fmod(ϕ + 2 * π, 2 * π)  # [0, 2π)
 
-        for z_spline_params, ϕ_spline_params in zip(
-            self.z_spline_params.split(1, dim=0),
-            self.ϕ_spline_params,
+        for i, (z_params, ϕ_params) in enumerate(
+            zip(
+                self.z_params.split(1, dim=0),
+                self.ϕ_params,
+            )
         ):
-            z, ldj_z = self.z_spline(z, z_spline_params.expand(*z.shape, -1))
-            ϕ, ldj_ϕ = self.ϕ_spline(ϕ, ϕ_spline_params(z).view(*ϕ.shape, -1))
+            z, ldj_z = self.z_transform(z, z_params.expand(*z.shape, -1))
+            ϕ, ldj_ϕ = self.ϕ_transform(ϕ, ϕ_params(z).view(*ϕ.shape, -1))
 
             ldj += ldj_z + ldj_ϕ
-            
-            ϕ = torch.fmod(ϕ + π, 2 * π)  # effecively shift spline fixed points by π
+
+            ϕ = self.apply_global_rotation(ϕ, i)
 
         u = torch.cos(ϕ)
         v = torch.sin(ϕ)
@@ -176,9 +213,77 @@ class NormalizingFlowRQS(NormalizingFlowBase):
         outputs.squeeze_(dim=1)
 
         return outputs, ldj
-    
 
-class NormalizingFlowC2(NormalizingFlowBase):
+
+class NormalizingFlowRQSMobius(NormalizingFlowBase):
+    def __init__(
+        self,
+        κ: float,
+        μ: Tensor,
+        n_layers: int,
+        n_spline: int,
+        n_mobius: int,
+        hidden_shape: list[int],
+        activation: str,
+        batch_size: int,
+        val_batch_size: int,
+        init_lr: float,
+    ):
+        super().__init__(κ, μ, n_layers, batch_size, val_batch_size, init_lr)
+
+        self.z_transform = RQSplineTransform([-1, 1], n_spline)
+        self.xy_transform = MobiusMixtureTransform(
+            n_mobius, weighted=True, epsilon=1e-3
+        )
+
+        self.z_params = torch.nn.Parameter(
+            torch.stack(
+                [self.z_transform.identity_params for _ in range(n_layers)],
+                dim=0,
+            )
+        )
+        self.xy_params = torch.nn.ModuleList(
+            [
+                simple_fnn_conditioner(
+                    in_features=1,
+                    out_features=self.xy_transform.n_params,
+                    hidden_shape=hidden_shape,
+                    activation=activation,
+                )
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, inputs: Tensor) -> tuple[Tensor, Tensor]:
+        xy, z = inputs.tensor_split([2], dim=-1)
+        ldj = torch.zeros(inputs.shape[0], device=inputs.device)
+
+        ρ = (1 - z**2).clamp(min=1e-12, max=1.0).sqrt()
+        xy = xy / ρ
+
+        for i, (z_params, xy_params) in enumerate(
+            zip(
+                self.z_params.split(1, dim=0),
+                self.xy_params,
+            )
+        ):
+            z, ldj_z = self.z_transform(z, z_params.expand(*z.shape, -1))
+            xy, ldj_xy = self.xy_transform(xy, xy_params(z).view(*xy.shape[:-1], -1))
+
+            ldj += ldj_z + ldj_xy
+
+            xy = self.apply_global_rotation(xy, i)
+
+        ρ = (1 - z**2).clamp(min=1e-12, max=1.0).sqrt()
+        xy = ρ * xy
+
+        outputs = torch.cat([xy, z], dim=-1)
+        outputs.squeeze_(dim=1)
+
+        return outputs, ldj
+
+
+class NormalizingFlowBSRQS(NormalizingFlowBase):
     def __init__(
         self,
         κ: float,
@@ -191,22 +296,22 @@ class NormalizingFlowC2(NormalizingFlowBase):
         val_batch_size: int,
         init_lr: float,
     ):
-        super().__init__(κ, μ, batch_size, val_batch_size, init_lr)
+        super().__init__(κ, μ, n_layers, batch_size, val_batch_size, init_lr)
 
-        self.spline_transform = C2SplineTransform([-1, 1], n_spline)
-        self.mobius_transform = MobiusTransform()
+        self.z_transform = BSplineTransform([-1, 1], n_spline)
+        self.ϕ_transform = RQSplineTransformCircularDomain(n_spline)
 
         self.z_params = torch.nn.Parameter(
             torch.stack(
-                [self.spline_transform.identity_params for _ in range(n_layers)],
+                [self.z_transform.identity_params for _ in range(n_layers)],
                 dim=0,
             )
         )
-        self.xy_params = torch.nn.ModuleList(
+        self.ϕ_params = torch.nn.ModuleList(
             [
                 simple_fnn_conditioner(
                     in_features=1,
-                    out_features=self.mobius_transform.n_params,
+                    out_features=self.ϕ_transform.n_params,
                     hidden_shape=hidden_shape,
                     activation=activation,
                 )
@@ -215,33 +320,99 @@ class NormalizingFlowC2(NormalizingFlowBase):
         )
 
     def forward(self, inputs: Tensor) -> tuple[Tensor, Tensor]:
+        x, y, z = inputs.split(1, dim=-1)
+        ldj = torch.zeros(inputs.shape[0], device=inputs.device)
 
+        ρ = (1 - z**2).clamp(min=1e-12, max=1.0).sqrt()
+        u = x / ρ
+        v = y / ρ
+        ϕ = torch.atan2(v, u)  # [-π, π)
+        ϕ = torch.fmod(ϕ + 2 * π, 2 * π)  # [0, 2π)
+
+        for i, (z_params, ϕ_params) in enumerate(
+            zip(
+                self.z_params.split(1, dim=0),
+                self.ϕ_params,
+            )
+        ):
+            z, ldj_z = self.z_transform(z, z_params.expand(*z.shape, -1))
+            ϕ, ldj_ϕ = self.ϕ_transform(ϕ, ϕ_params(z).view(*ϕ.shape, -1))
+
+            ldj += ldj_z + ldj_ϕ
+
+            ϕ = self.apply_global_rotation(ϕ, i)
+
+        u = torch.cos(ϕ)
+        v = torch.sin(ϕ)
+
+        ρ = (1 - z**2).clamp(min=1e-12, max=1.0).sqrt()
+        x = ρ * u
+        y = ρ * v
+
+        outputs = torch.cat([x, y, z], dim=-1)
+        outputs.squeeze_(dim=1)
+
+        return outputs, ldj
+
+
+class NormalizingFlowBSMobius(NormalizingFlowBase):
+    def __init__(
+        self,
+        κ: float,
+        μ: Tensor,
+        n_layers: int,
+        n_spline: int,
+        n_mobius: int,
+        hidden_shape: list[int],
+        activation: str,
+        batch_size: int,
+        val_batch_size: int,
+        init_lr: float,
+    ):
+        super().__init__(κ, μ, n_layers, batch_size, val_batch_size, init_lr)
+
+        self.z_transform = BSplineTransform([-1, 1], n_spline)
+        self.xy_transform = MobiusMixtureTransform(
+            n_mobius, weighted=True, epsilon=1e-3
+        )
+
+        self.z_params = torch.nn.Parameter(
+            torch.stack(
+                [self.z_transform.identity_params for _ in range(n_layers)],
+                dim=0,
+            )
+        )
+        self.xy_params = torch.nn.ModuleList(
+            [
+                simple_fnn_conditioner(
+                    in_features=1,
+                    out_features=self.xy_transform.n_params,
+                    hidden_shape=hidden_shape,
+                    activation=activation,
+                )
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, inputs: Tensor) -> tuple[Tensor, Tensor]:
         xy, z = inputs.tensor_split([2], dim=-1)
         ldj = torch.zeros(inputs.shape[0], device=inputs.device)
 
         ρ = (1 - z**2).clamp(min=1e-12, max=1.0).sqrt()
         xy = xy / ρ
 
-        for z_params, xy_params in zip(
-            self.z_params.split(1, dim=0),
-            self.xy_params,
+        for i, (z_params, xy_params) in enumerate(
+            zip(
+                self.z_params.split(1, dim=0),
+                self.xy_params,
+            )
         ):
-            z, ldj_z = self.spline_transform(z, z_params.expand(*z.shape, -1))
-            
-            # [-1, 1]
-            omega_x, omega_y = torch.tanh(xy_params(z)).clamp(-1 + 1e-3, 1 - 1e-3).view(*xy.shape).split(1, dim=-1)
-            omega_y = omega_y * torch.sqrt(1 - omega_x.pow(2))  # [-sqrt(1 - x^2), sqrt(1 - x^2)]
-            omega = torch.cat([omega_x, omega_y], dim=-1)
-            xy, ldj_xy = self.mobius_transform(xy, omega)
-            
-            #print(omega.pow(2).sum(dim=-1).abs().max())
-
-            #print("z: ", z.isnan().any())
-            #print("xy: ", xy.isnan().any())
-            #print("ldj_z: ", ldj_z.isnan().any())
-            #print("ldj_xy: ", ldj_xy.isnan().any())
+            z, ldj_z = self.z_transform(z, z_params.expand(*z.shape, -1))
+            xy, ldj_xy = self.xy_transform(xy, xy_params(z).view(*xy.shape[:-1], -1))
 
             ldj += ldj_z + ldj_xy
+
+            xy = self.apply_global_rotation(xy, i)
 
         ρ = (1 - z**2).clamp(min=1e-12, max=1.0).sqrt()
         xy = ρ * xy
@@ -256,6 +427,5 @@ class DummyNormalizingFlow(pl.LightningModule):
     def __init__(self):
         super().__init__()
 
-        
     def forward(self, inputs: Tensor) -> tuple[Tensor, Tensor]:
         return inputs, torch.zeros(inputs.shape[0], device=inputs.device)

transforms.py
@@ -4,50 +4,100 @@ import math
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
+import torch.linalg as LA
 
 Tensor = torch.Tensor
 
 PI = math.pi
 
 
-class MobiusTransform:
-    """
-    Mobius transform from the circle to itself.
+class MobiusMixtureTransform:
+    def __init__(
+        self, n_mixture: int, weighted: bool = True, epsilon: float = 1e-3
+    ) -> None:
+        self.n_mixture = n_mixture
+        self.weighted = weighted and n_mixture > 1
+        self.epsilon = epsilon
 
-        -y = (1 - |ω|^2) / |x - ω|^2 * (x - ω) - ω
-        
-        |dy/dx| = (1 - |ω|^2) / |x - ω|^2
-    
-    The transformation takes a batch of inputs `x` with shape
-    (n_batch, ..., 2) that are (x, y) coordinates on the unit circle,
-    and a batch of parameters `omega` with the same shape, but the
-    (x, y) coordinates live on the unit disk. The transformation
-    returns coordinates `y` on the unit circle.
-    """
-    
     @property
     def identity_params(self) -> Tensor:
-        return torch.tensor([0.0, 0.0])
-    
+        return torch.zeros(self.n_params)
+
     @property
     def n_params(self) -> int:
-        return 2
-    
-    def forward(self, x: Tensor, omega: Tensor) -> tuple[Tensor, Tensor]:
-        assert x.shape[-1] == 2
-        assert x.shape == omega.shape
-        #assert torch.allclose(x.pow(2).sum(dim=-1), torch.ones(x.shape[:-1]))
-        assert torch.all(omega.pow(2).sum(dim=-1) < 1), f"{omega.pow(2).sum(dim=-1).abs().max()}"
-        
-        coeff = (1 - omega.pow(2).sum(dim=-1, keepdim=True)) / (x - omega).pow(2).sum(dim=-1, keepdim=True)
-        y = - (coeff * (x - omega) - omega)
-        ldj = torch.log(coeff).flatten(start_dim=1).sum(dim=1)
-        
-        return y, ldj
-    
+        return self.n_mixture * (2 + int(self.weighted))
+
+    def _constrain_parameters(self, params: Tensor) -> tuple[Tensor, Tensor]:
+        params = params.view(*params.shape[:-1], self.n_mixture, -1)
+        if params.shape[-1] == 3:
+            omega, rho = params.split([2, 1], dim=-1)
+            rho = torch.softmax(rho, dim=-2)
+        elif params.shape[-1] == 2:
+            omega = params
+            rho = 1 / self.n_mixture
+        else:
+            raise ValueError("Wrong number of parameters provided")
+
+        omega = torch.tanh(omega) * (1 - self.epsilon)
+        omega1, omega2 = omega.split(1, dim=-1)
+        omega = torch.cat([omega1, omega2 * (1 - omega1.pow(2)).sqrt()], dim=-1)
+
+        return omega, rho
+
+    def forward(self, x: Tensor, params: Tensor) -> tuple[Tensor, Tensor]:
+        *data_shape, n_coords = x.shape
+        assert n_coords == 2
+        assert params.shape == torch.Size([*data_shape, self.n_params])
+
+        omega, rho = self._constrain_parameters(params)
+
+        assert torch.all(
+            omega.pow(2).sum(dim=-1) < 1
+        ), f"{omega.pow(2).sum(dim=-1).abs().max()}"
+
+        x.unsqueeze_(dim=-2)
+        x_10 = torch.tensor([1.0, 0.0], device=x.device).view(
+            *[1 for _ in data_shape], 1, 2
+        )
+
+        assert x.shape == torch.Size([*data_shape, 1, 2])
+        if type(rho) is Tensor:
+            assert rho.shape == torch.Size([*data_shape, self.n_mixture, 1])
+        assert omega.shape == torch.Size([*data_shape, self.n_mixture, 2])
+
+        one_minus_modsq_omega = 1 - omega.pow(2).sum(dim=-1, keepdim=True)
+        modsq_x_minus_omega = (x - omega).pow(2).sum(dim=-1, keepdim=True)
+        modsq_x_10_minus_omega = (x_10 - omega).pow(2).sum(dim=-1, keepdim=True)
+        vol_factor = one_minus_modsq_omega / modsq_x_minus_omega
+        vol_factor_10 = one_minus_modsq_omega / modsq_x_10_minus_omega
+
+        g = vol_factor * (x - omega) - omega
+        g_10 = vol_factor_10 * (x_10 - omega) - omega
+
+        g1, g2 = g.split(1, dim=-1)
+        g1_10, g2_10 = g_10.split(1, dim=-1)
+
+        # Rotate such that g((1, 0)) = (1, 0)
+        g1_rot = g1_10 * g1 + g2_10 * g2
+        g2_rot = -g2_10 * g1 + g1_10 * g2
+
+        if self.n_mixture == 1:
+            y = torch.cat([g1_rot, g2_rot], dim=-1).squeeze(dim=-2)
+            log_delta_vol = vol_factor.log().flatten(start_dim=1).sum(dim=1)
+        else:
+            theta = torch.fmod(torch.atan2(g2_rot, g1_rot) + 2 * PI, 2 * PI)
+            theta_mean = (rho * theta).sum(dim=-2)
+            y = torch.cat([theta_mean.cos(), theta_mean.sin()], dim=-1)
+            log_delta_vol = (
+                (rho * vol_factor).sum(dim=-2).log().flatten(start_dim=1).sum(dim=1)
+            )
+
+        assert y.shape == torch.Size([*data_shape, 2])
+
+        return y, log_delta_vol
+
     def __call__(self, x, params):
         return self.forward(x, params)
-        
 
 
 class RQSplineTransform:
@@ -204,40 +254,33 @@ class RQSplineTransformCircularDomain(RQSplineTransform):
             *derivs.shape[:-1], -1
         )
 
-class C2SplineTransform:
-    
+
+class BSplineTransform:
     def __init__(self, interval: tuple[float], n_segments: int) -> None:
-        #self.a, self.b = interval
         self.interval = interval
         self.n_segments = n_segments
-    
+
     @property
     def identity_params(self) -> Tensor:
         return torch.ones(self.n_segments * 2 + 2)
-    
+
     def build_spline(
         self,
         intervals: Tensor,
         weights: Tensor,
     ) -> tuple[Tensor]:
-        
-        #Δ = F.softplus(intervals) + 1e-3  # 
+        # Δ = F.softplus(intervals) + 1e-3
         Δ = torch.sigmoid(intervals) + 0.1
         Δ = Δ / Δ.sum(dim=-1, keepdim=True)
         Δpad = F.pad(Δ, (1, 1), "constant", 0)
-        
-        #print(f"Δ = {Δ.shape}")
-        
-        ρ = F.softplus(weights) + 1e-3  # min weight TODO make configurable
-        ρ = ρ / (((ρ[..., :-2] + ρ[..., 1:-1] + ρ[..., 2:]) / 3) * Δ).sum(dim=-1, keepdim=True)
-        
-        ω = (ρ[..., 1:] - ρ[..., :-1]) / (Δpad[..., :-1] + Δpad[..., 1:])
-        I = ρ[..., 1:-1] * Δ + (1/3) * (ω[..., 1:] - ω[..., :-1]) * Δ ** 2
-        
-        #print(f"ρ = {ρ.shape}")
-        #print(f"ω = {ω.shape}")
 
-        #Y = np.concatenate(([a,], a + np.cumsum(I, dim=-1)))
+        ρ = F.softplus(weights) + 1e-3  # min weight TODO make configurable
+        ρ = ρ / (((ρ[..., :-2] + ρ[..., 1:-1] + ρ[..., 2:]) / 3) * Δ).sum(
+            dim=-1, keepdim=True
+        )
+
+        ω = (ρ[..., 1:] - ρ[..., :-1]) / (Δpad[..., :-1] + Δpad[..., 1:])
+        I = ρ[..., 1:-1] * Δ + (1 / 3) * (ω[..., 1:] - ω[..., :-1]) * Δ**2
 
         zeros = torch.zeros_like(I).sum(dim=-1, keepdim=True)
 
@@ -255,31 +298,22 @@ class C2SplineTransform:
             ),
             dim=-1,
         )
-        #print(f"X = {X.shape}")
-        #print(f"Y = {Y.shape}")
-        
+
         return Δpad, ρ, ω, X, Y
-    
+
     def forward(self, x: Tensor, params: Tensor) -> tuple[Tensor, Tensor]:
         x = x.contiguous()
-        #params = params.unsqueeze(dim=1)  # shape (batch, 1, n_params)
-        
-        #print("x: ", x.min(), x.max())
+
         x = x - self.interval[0]
         x = x / (self.interval[1] - self.interval[0])
-        
+
         intervals, weights = params.split(
             (self.n_segments, self.n_segments + 2),
             dim=-1,
         )
         Δ, ρ, ω, X, Y = self.build_spline(intervals, weights)
-        #print(f"Δ = {Δ[0].tolist()},  ρ = {ρ[0].tolist()}")
-        
-        #print(f"x = {x.shape}")
-
 
         ix = torch.searchsorted(X, x.unsqueeze(-1), side="right")
-        #print(ix.min(), ix.max())
         ix.clamp_(1, self.n_segments)
 
         # Get parameters of the segments that x falls in
@@ -289,38 +323,18 @@ class C2SplineTransform:
         ωim1 = torch.gather(ω, -1, ix - 1).squeeze(-1)
         x0 = torch.gather(X, -1, ix - 1).squeeze(-1)
         y0 = torch.gather(Y, -1, ix - 1).squeeze(-1)
-        
-        #if not torch.all(x > x0):
-        #    print(f"x = {x[~(x > x0)]}, x0 = {x0[~(x > x0)]}")
-        #if not torch.all(x < x0 + Δ):
-        #    print(f"x = {x[~(x < x0 + Δ)]}, x0 + Δ = {(x0 + Δ)[~(x < x0 + Δ)]}")
 
         θ = (x - x0) / Δ
-        
-        #if not torch.all(θ >= 0):
-        #    print(f"θ = {θ[~(θ >= 0)]}")
-        #if not torch.all(θ < 1):
-        #    print(f"θ = {θ[~(θ < 1)]}")
 
-        #alpha.clamp_(1e-4, 1 - 1e-4)
-        
         y = (
             y0
             + ρ * Δ * θ
-            - ωim1 * Δ ** 2 * θ * (1 - θ)
-            + (1/3) * (ωi - ωim1) * Δ ** 2 * θ ** 3
+            - ωim1 * Δ**2 * θ * (1 - θ)
+            + (1 / 3) * (ωi - ωim1) * Δ**2 * θ**3
         )
-        
-        #print("X: ", X[0])
-        #print("Y: ", Y[0])
-        #print("x: ", x.min(), x.max())
-        #print("y: ", y.min(), y.max())
-        gradient = (
-            ρ
-            + ωi * Δ * θ ** 2
-            - ωim1 * Δ * (1 - θ) ** 2
-        )
-        
+
+        gradient = ρ + ωi * Δ * θ**2 - ωim1 * Δ * (1 - θ) ** 2
+
         if gradient.less(0.0).any():
             values = gradient[gradient < 0]
             print(f"Gradient has negative values: {values.tolist()}")
@@ -333,16 +347,11 @@ class C2SplineTransform:
             gradient = gradient.clamp(min=1e-6)
 
         ldj = gradient.log().flatten(start_dim=1).sum(dim=1)
-        
+
         y = y * (self.interval[1] - self.interval[0])
         y = y + self.interval[0]
-        ldj += math.log(self.interval[1] - self.interval[0]) * torch.numel(y[0])
-        
-        #print("y: ", y.min(), y.max())
 
         return y, ldj
 
     def __call__(self, x: Tensor, params: Tensor) -> tuple[Tensor, Tensor]:
         return self.forward(x, params)
-
-

visualisations.py
@@ -14,103 +14,111 @@ sns.set_theme()
 
 
 def pairplot(xyz: Tensor) -> sns.PairGrid:
-    
     df = pd.DataFrame(xyz, columns=["x", "y", "z"])
 
     grid = sns.pairplot(df, kind="hist", corner=True)
-    [(ax.set_xlim(-1.1, 1.1), ax.set_ylim(-1.1, 1.1)) for ax in grid.axes.flatten() if ax is not None]
-    
+    [
+        (ax.set_xlim(-1.1, 1.1), ax.set_ylim(-1.1, 1.1))
+        for ax in grid.axes.flatten()
+        if ax is not None
+    ]
+
     return grid
 
 
 def heatmap(xyz: Tensor, bins: int = 50, **pcolormesh_kwargs) -> plt.Figure:
-    
-    x, y, z = xyz.flatten(end_dim=-2).split(1, dim=-1) # merges all batches
+    x, y, z = xyz.flatten(end_dim=-2).split(1, dim=-1)  # merges all batches
     θϕ = torch.cat([torch.asin(z), torch.atan2(y, x)], dim=-1)
-    θ_grid = torch.linspace(-π/2, π/2, bins + 1)
+    θ_grid = torch.linspace(-π / 2, π / 2, bins + 1)
     ϕ_grid = torch.linspace(-π, π, bins + 1)
-    
+
     hist, _ = torch.histogramdd(θϕ, bins=[θ_grid, ϕ_grid], density=True)
-    
+
     fig, ax = plt.subplots(subplot_kw=dict(projection="mollweide"))
     ax.set_yticklabels([])
     ax.set_xticklabels([])
-    
+
     pcolormesh_kwargs = (
-        dict(cmap="viridis", shading='auto', vmin=0, vmax=1) | pcolormesh_kwargs
+        dict(cmap="viridis", shading="auto", vmin=0, vmax=1) | pcolormesh_kwargs
     )
-        
+
     ax.pcolormesh(
         ϕ_grid,
         θ_grid,
         hist,
         **pcolormesh_kwargs,
     )
-    
+
     fig.tight_layout()
-    
+
     return fig
+
 
 def line(xyz: Tensor, figsize: tuple = (6, 6), **plot_kwargs) -> plt.Figure:
-    
     x, y, z = xyz.split(1, dim=-1)
     θ = torch.asin(z)
     ϕ = torch.atan2(y, x)
     θϕ = torch.cat([θ, ϕ], dim=-1)
-    
+
     fig, ax = plt.subplots(subplot_kw=dict(projection="mollweide"))
     ax.set_yticklabels([])
     ax.set_xticklabels([])
-    
+
     ax.plot(ϕ, θ, **plot_kwargs)
-    
-    fig.tight_layout()    
-    
+
+    fig.tight_layout()
+
     return fig
 
+
 def scatter(xyz: Tensor, figsize: tuple = (6, 6), **scatter_kwargs) -> plt.Figure:
-    
     x, y, z = xyz.split(1, dim=-1)
     θ = torch.asin(z)
     ϕ = torch.atan2(y, x)
     θϕ = torch.cat([θ, ϕ], dim=-1)
-    
+
     fig, ax = plt.subplots(subplot_kw=dict(projection="mollweide"))
     ax.set_yticklabels([])
     ax.set_xticklabels([])
-    
+
     ax.scatter(ϕ, θ, **scatter_kwargs)
-    
-    fig.tight_layout()    
-    
+
+    fig.tight_layout()
+
     return fig
 
 
 def scatter3d(xyz: Tensor, **scatter3D_kwargs) -> plt.Figure:
     fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
     ax.plot_surface(*spherical_mesh(25), rstride=1, cstride=1, alpha=0.2, lw=0.1)
-    
+
     xyz = xyz.unsqueeze(dim=0).flatten(start_dim=0, end_dim=-3)
     for batch in xyz.split(1, dim=0):
         x, y, z = batch.split(1, dim=-1)
         ax.scatter3D(x.squeeze(), y.squeeze(), z.squeeze(), **scatter3D_kwargs)
-    
+
     ax.set_axis_off()
     fig.tight_layout()
-    
+
     return fig
+
 
 def line3d(xyz: Tensor, hide_axis: bool = True, **plot3D_kwargs) -> plt.Figure:
     fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
-    
+
     ax.plot_surface(*spherical_mesh(25), rstride=1, cstride=1, alpha=0.2, lw=0.1)
-    
+
     xyz = xyz.unsqueeze(dim=0).flatten(start_dim=0, end_dim=-3)
     for batch in xyz.split(1, dim=0):
         x, y, z = batch.split(1, dim=-1)
-        ax.plot3D(x.squeeze().tolist(), y.squeeze().tolist(), z.squeeze().tolist(), **plot3D_kwargs)
-    
+        ax.plot3D(
+            x.squeeze().tolist(),
+            y.squeeze().tolist(),
+            z.squeeze().tolist(),
+            **plot3D_kwargs,
+        )
+
     ax.set_axis_off()
     fig.tight_layout()
-    
+
     return fig